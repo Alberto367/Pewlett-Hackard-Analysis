39320ff12653cb927d3828c47ec7b5ba15edfb20
Alberto Garcia <alberto_gar97@hotmail.com>
2022-04-02 11:48:05 -0700

    Created Retirement Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-
-
 -- Creating tables for week7_module
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -39,14 +37,14 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
+    emp_no int NOT NULL,
 	dept_no varchar NOT NULL,
-	emp_no int NOT NULL,
 	from_date date NOT NULL,
 	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -56,7 +54,46 @@ CREATE TABLE titles (
 	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM titles;
+
+SELECT COUNT(*)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT COUNT(*)
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
